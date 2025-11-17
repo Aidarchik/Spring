95acf88072145bf6b903f5237f9e2d211243081e
Aidarchik <ishimgrad@gmail.com>
2025-11-17 14:03:41 +0300

    Предварительная связка Spring MVC+Hibernate

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,32 @@
+package web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/users")
+    public String printWelcome(ModelMap model) {
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        model.addAttribute("messages", userService.listUsers());
+        return "index";
+    }
+
+}
